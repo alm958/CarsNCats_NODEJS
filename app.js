aadd14d6a811f56757fba1f58a8786aaaf8a64e1
alm958 <alm958@gmail.com>
2016-11-14 18:47:39 -0500

    updated from switch case to if

--- app.js
+++ app.js
@@ -33,57 +33,29 @@ function loadJpg(image, response, code){
 
 var server = http.createServer(function (request, response){
     console.log(request.url);
-    switch (request.url)
-    {
-        case '/':
-            loadPage('views/index.html', response, 200);
-            break;
-        case '/cars':
-            loadPage('views/cars.html', response, 200);
-            break;
-        case '/cats':
-            loadPage('views/cats.html', response, 200);
-            break;
-        case '/cars/new':
-            loadPage('views/newcars.html', response, 200);
-            break;
-        case '/stylesheets/style.css':
-            serveCSS('./stylesheets/style.css', response, 200);
-            break;
-        case '/images/cars01.jpg':
-            loadJpg('./images/cars01.jpg', response, 200);
-            break;
-        case '/images/cars02.jpg':
-            loadJpg('./images/cars02.jpg', response, 200);
-            break;
-        case '/images/cars03.jpg':
-            loadJpg('./images/cars03.jpg', response, 200);
-            break;
-        case '/images/cars04.jpg':
-            loadJpg('./images/cars04.jpg', response, 200);
-            break;
-        case '/images/cars05.jpg':
-            loadJpg('./images/cars05.jpg', response, 200);
-            break;
-        case '/images/cats01.jpg':
-            loadJpg('./images/cats01.jpg', response, 200);
-            break;
-        case '/images/cats02.jpg':
-            loadJpg('./images/cats02.jpg', response, 200);
-            break;
-        case '/images/cats03.jpg':
-            loadJpg('./images/cats03.jpg', response, 200);
-            break;
-        case '/images/cats04.jpg':
-            loadJpg('./images/cats04.jpg', response, 200);
-            break;
-        case '/images/cats05.jpg':
-            loadJpg('./images/cats05.jpg', response, 200);
-            break;
-        default:
-            loadPage('error.html', response, 404);
-            break;
-    };
+    if (request.url === '/'){
+        loadPage('views/index.html', response, 200);
+    }
+    else if (request.url === '/cars') {
+        loadPage('views/cars.html', response, 200);
+    }
+    else if (request.url === '/cats') {
+        loadPage('views/cats.html', response, 200);
+    }
+    else if (request.url === '/cars/new') {
+        loadPage('views/newcars.html', response, 200);
+    }
+    else if (request.url.endsWith('.jpg')) {
+        console.log(request.url);
+        loadJpg(`.${request.url}`, response, 200);
+    }
+    else if (request.url.endsWith('.css')) {
+        serveCSS(`.${request.url}`, response, 200);
+    }
+    else {
+        loadPage('views/error.html', response, 404);
+    }
+
 });
 
 server.listen(6789);
